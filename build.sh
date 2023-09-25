7e0fcb608d781f8b1445b01bc1f474c2d08c6c22
Mahito1408 <maholiandrade12@gmail.com>
2023-09-25 10:54:52 -0500

    edit home

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-DJANGO_SUPERUSER_PASSWORD=Mahito148 python manage.py createsuperuser --no-input --username=maholyandrade  --email=maholiandrade12@gmail.com
+DJANGO_SUPERUSER_PASSWORD=Mahito148 python manage.py createsuperuser --no-input --username=maholyandrade1  --email=maholiandrade12@gmail.com
